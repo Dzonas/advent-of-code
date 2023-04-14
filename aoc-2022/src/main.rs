f07ea69ac0ed3d495c3ed268eca6ad74cb0e3d76
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2023-04-14 18:58:19 +0200

    Rework main to be cli solution runner

--- aoc-2022/src/main.rs
+++ aoc-2022/src/main.rs
@@ -1,19 +1,39 @@
 mod year_2022;
 
-use year_2022::day_01::{solve_part_1, solve_part_2};
+use std::env;
+use std::io;
+use std::io::Read;
 
 fn main() {
-    let input_2022_01 = include_str!("../input/2022/01.txt");
-    let input_2022_02 = include_str!("../input/2022/02.txt");
+    let mut args = env::args();
 
-    let part_1_solution = solve_part_1(input_2022_01);
-    let part_2_solution = solve_part_2(input_2022_01);
+    let _ = args.next();
+    let year: u32 = args.next().unwrap().parse().unwrap();
+    let day: u32 = args.next().unwrap().parse().unwrap();
 
-    let day_02_part_1_solution = year_2022::day_02::solve_part_1(input_2022_02);
-    let day_02_part_2_solution = year_2022::day_02::solve_part_2(input_2022_02);
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    let (part_1_solution, part_2_solution) = match (year, day) {
+        (2022, 1) => {
+            use year_2022::day_01::{solve_part_1, solve_part_2};
+
+            let part_1_solution = solve_part_1(&input);
+            let part_2_solution = solve_part_2(&input);
+
+            (part_1_solution, part_2_solution)
+        }
+        (2022, 2) => {
+            use year_2022::day_02::{solve_part_1, solve_part_2};
+
+            let part_1_solution = solve_part_1(&input);
+            let part_2_solution = solve_part_2(&input);
+
+            (part_1_solution, part_2_solution)
+        }
+        _ => panic!("Not solved yet"),
+    };
 
     println!("{}", part_1_solution);
     println!("{}", part_2_solution);
-    println!("{}", day_02_part_1_solution);
-    println!("{}", day_02_part_2_solution);
 }
